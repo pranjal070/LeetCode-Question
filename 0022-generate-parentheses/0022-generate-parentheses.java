1f0f504e578c244366cdf0f3a41c4672fbcae504
pranjal070 <pranjalthaku07@gmail.com>
2026-08-11 17:40:44 +0530

    Time: 2 ms (70.55%), Space: 44.9 MB (33.3%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<String> ans=new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        backtrack("",0,0,n);
+        return ans;
+        
+    }
+       public void backtrack(String s, int open, int close, int n){
+        if(s.length()==2*n){
+            ans.add(s);
+            return;
+        }
+        if(open<n){
+            backtrack(s+"(",open+1,close,n);
+        }
+        if(close<open){
+            backtrack(s+")",open,close+1,n);
+        }
+
+    }
+}
