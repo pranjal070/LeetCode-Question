4d6a02646f7b957b6ba8a26a26179b3caa2a9e9a
pranjal070 <pranjalthaku07@gmail.com>
2026-08-13 12:26:02 +0530

    Time: 28 ms (40.6%), Space: 92.2 MB (41.25%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,32 @@
+
+
+
+
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {       
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {               
+//                 if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
+//                     return true;
+//                 }
+//             }
+//         }       
+//         return false;
+//     }
+// }
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
