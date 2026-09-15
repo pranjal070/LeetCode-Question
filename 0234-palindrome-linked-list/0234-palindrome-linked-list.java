d4f4addbe3ecb488e9c9a946affa061436a4ebe3
pranjal070 <pranjalthaku07@gmail.com>
2026-09-15 11:44:38 +0530

    Time: 4 ms (67.21%), Space: 94.2 MB (79.39%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -4,8 +4,6 @@ class Solution {
         if (head == null || head.next == null) {
             return true;
         }
-
-        // Find middle
         ListNode slow = head;
         ListNode fast = head;
 
@@ -13,31 +11,23 @@ class Solution {
             slow = slow.next;
             fast = fast.next.next;
         }
-
-        // Reverse second half
         ListNode prev = null;
         ListNode curr = slow;
-
         while (curr != null) {
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
         }
-
-        // Compare
         ListNode first = head;
         ListNode second = prev;
-
         while (second != null) {
             if (first.val != second.val) {
                 return false;
             }
-
             first = first.next;
             second = second.next;
         }
-
         return true;
     }
 }
